4e13b8bbfa44f5b7f2504085a866bf25517f5297
Gavin Adams <gavinaws@amazon.com>
2019-02-08 09:25:06 -0700

    and one more pathlib detail

--- codebuild/publish_packages.py
+++ codebuild/publish_packages.py
@@ -192,7 +192,7 @@ class Package:
                 'zip',
                 str(Path('/tmp/{}-{}'.format(package_list, runtime)))
             )
-            result[package_list+'-'+runtime] = Path('/tmp/{}-{}.zip'.format(package_list, runtime))
+            result[package_list+'-'+runtime] = str(Path('/tmp/{}-{}.zip'.format(package_list, runtime)))
             shutil.rmtree(str(Path('/tmp/{}-{}'.format(package_list, runtime))))
 
         # Return runtimes and file names in Path format
